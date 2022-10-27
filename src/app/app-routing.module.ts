f226964eebd59c3994b52b0148f9d7a688dc7c5a
Haminton Mena Mena <hamintonjair@gmail.com>
2022-10-27 15:13:09 -0500

    se creo la vista del  login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path : "principal", component: PrincipalComponent },
+ { path : "**",component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
