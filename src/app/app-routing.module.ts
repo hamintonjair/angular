bacb4d0792013f566b4b750139d8758b965479be
Haminton Mena Mena <hamintonjair@gmail.com>
2022-10-27 23:23:26 -0500

    se crearon los botones para los módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   
-  { path : 'login', component: LoginComponent },
+  { path : '', component: LoginComponent },
   { path : 'principal', component: PrincipalComponent },
   { path : "**",component: PageNotFoundComponent }
 ];
