7c7a25371d90d17197187216517531bae567d79a
Haminton Mena Mena <hamintonjair@gmail.com>
2022-10-27 22:06:18 -0500

    mejora del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { PrincipalComponent } from './components/principal/principal.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
- { path : "principal", component: PrincipalComponent },
- { path : "**",component: PageNotFoundComponent }
+  
+  { path : 'login', component: LoginComponent },
+  { path : 'principal', component: PrincipalComponent },
+  { path : "**",component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
